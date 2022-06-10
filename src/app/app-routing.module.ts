a9363b365213030e11b4178b996e88e5af05b138
Mei Yihang <meiyihang1@gmail.com>
2022-06-10 15:19:18 +0200

    fix a bug in navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from 'src/pages/home/home.component';
 import { KidsComponent } from 'src/pages/kids/kids.component';
 import { MenComponent } from 'src/pages/men/men.component';
 import { WomenComponent } from 'src/pages/women/women.component';
+import { FormComponentComponent } from './form-component/form-component/form-component.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
@@ -14,7 +15,8 @@ export const routes: Routes = [
     { path: 'men', component: MenComponent },
     { path: 'women', component: WomenComponent },
     { path: 'kids', component: KidsComponent },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: FormComponentComponent}
 ];
 
 @NgModule({
