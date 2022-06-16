d20c4e1121efffb12227bedc4fc2c749ce8e7804
MohamedAboMousallam <86533055+MohamedAboMousallam@users.noreply.github.com>
2022-06-17 01:30:24 +0200

    product card added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
+import { ContactusComponent } from 'src/pages/contactus/contactus.component';
+import { HomeComponent } from 'src/pages/home/home.component';
+import { KidsComponent } from 'src/pages/kids/kids.component';
+import { MenComponent } from 'src/pages/men/men.component';
+import { OurpoliciesComponent } from 'src/pages/ourpolicies/ourpolicies.component';
+import { WomenComponent } from 'src/pages/women/women.component';
+import { FormComponentComponent } from './form-component/form-component/form-component.component';
+import { LoginComponent } from './login/login.component';
+import { ProductComponent } from './product/product.component';
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full' },
+    { path: 'men', component: MenComponent },
+    { path: 'women', component: WomenComponent },
+    { path: 'kids', component: KidsComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'signup', component: FormComponentComponent},
+    { path: 'contactus', component: ContactusComponent },
+    { path: 'ourpolicies', component: OurpoliciesComponent }
+    { path: 'products', component: ProductComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,45 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from 'src/pages/home/home.component';
+import { MenComponent } from 'src/pages/men/men.component';
+import { WomenComponent } from 'src/pages/women/women.component';
+import { KidsComponent } from 'src/pages/kids/kids.component';
+import { LoginComponent } from './login/login.component';
+import { FormComponentComponent } from './form-component/form-component/form-component.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ContactusComponent } from 'src/pages/contactus/contactus.component';
+import { OurpoliciesComponent } from 'src/pages/ourpolicies/ourpolicies.component';
+import { ProductComponent } from './product/product.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    MenComponent,
+    WomenComponent,
+    KidsComponent,
+    ContactusComponent,
+    OurpoliciesComponent,
+    LoginComponent,
+    FormComponentComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
